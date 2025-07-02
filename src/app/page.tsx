eb81b45cc5c6ed2d882012a35fbcf38532a61cc2
Amandeep <eng.aman29@gmail.com>
2025-07-03 05:14:30 +0530

    ai setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,11 @@
 import { Button } from "@/components/ui/button"
 import { useTRPC } from "@/trpc/client"
 import { useMutation } from "@tanstack/react-query";
+import { useState} from "react"
+import {Input} from "@/components/ui/input"
 import {toast} from "sonner"
 const Page= ()=>{
+  const[value,setValue]=useState("")
   const trpc = useTRPC();
   const invoke = useMutation(trpc.invoke.mutationOptions({
     onSuccess:()=>{
@@ -12,7 +15,8 @@ const Page= ()=>{
   }));
   return (
    <div className="p-4 max-w-7xl mx-auto">
-    <Button onClick={()=>invoke.mutate({text:"Aman"})}>
+    <Input value={value} onChange={(e)=>setValue(e.target.value)}/>
+    <Button onClick={()=>invoke.mutate({value : value})}>
       Invoke the inngest function.
     </Button>
     
--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,12 +1,21 @@
 import { inngest } from "./client";
+import { Agent, gemini, createAgent } from "@inngest/agent-kit";
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
   { event: "test/hello.world" },
-  async ({ event, step }) => {
-    await step.sleep("wait-a-moment", "5s");
-     await step.sleep("wait-a-moment", "10s");
-      await step.sleep("wait-a-moment", "16s");
-    return { message: `Hello ${event.data.email}!` };
+  async ({ event }) => {
+    const codeAgent = createAgent({
+      name: "code-agent",
+      system: "[JUST NEED CODE , NOTHING ELSE , NO EXPLAINATION , NO COMMENTS]You are an expert nextjs developer. You write readable , maintainable code. You write simple Next.js and React snippets",
+      model: gemini({ model: "gemini-2.5-pro"}),
+    });
+  
+    const { output } = await codeAgent.run(
+                `Write the following snippet : ${event.data.value}`,
+                );
+    console.log(output);
+
+    return { output };
   },
 );

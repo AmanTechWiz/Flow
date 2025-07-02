c96b184ef86c76da4d6bb77c5238d94548ef31e9
Amandeep <eng.aman29@gmail.com>
2025-07-02 23:21:37 +0530

    inngest setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,22 @@
-import {getQueryClient, trpc} from "@/trpc/server"
-import {dehydrate, HydrationBoundary} from "@tanstack/react-query";
-import {Client} from "./client";
-import { Suspense } from "react";
-
-const Page=async ()=>{
-  const queryClient = getQueryClient();
-  void queryClient.prefetchQuery(
-    trpc.hello.queryOptions({text : "prefetch data..."})
-  )
-  console.log("client side")
+"use client"
+import { Button } from "@/components/ui/button"
+import { useTRPC } from "@/trpc/client"
+import { useMutation } from "@tanstack/react-query";
+import {toast} from "sonner"
+const Page= ()=>{
+  const trpc = useTRPC();
+  const invoke = useMutation(trpc.invoke.mutationOptions({
+    onSuccess:()=>{
+      toast.success("Bg jobs start!");
+    }
+  }));
   return (
-   <HydrationBoundary state={dehydrate(queryClient)}>
-    <Suspense fallback={<p>Load...</p>}>
-      <Client/>
-    </Suspense>
-   </HydrationBoundary>
+   <div className="p-4 max-w-7xl mx-auto">
+    <Button onClick={()=>invoke.mutate({text:"Aman"})}>
+      Invoke the inngest function.
+    </Button>
+    
+   </div>
   )
 }
 
